73afad94ed5ed475e2d5aa74bd12aba51819063e
sunnykushwahathakur <sunnykushwahathakur@gmail.com>
2025-03-22 14:48:41 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className='w-full h-screen' style={{backgroundColor: color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <button className='bg-red-500 px-2 py-1 rounded-full text-white cursor-pointer' onClick={() => setColor("red")} >Red</button>
+            <button className='bg-red-500 px-2 py-1 rounded-full text-white cursor-pointer' onClick={() => setColor("red")} >Reded</button>
             <button className='bg-blue-500 px-2 py-1 rounded-full text-white cursor-pointer' onClick={() => setColor("blue")} >Blue</button>
             <button className='bg-green-500 px-2 py-1 rounded-full text-white cursor-pointer' onClick={() => setColor("green")} >Green</button>
             <button className='bg-yellow-500 px-2 py-1 rounded-full text-white cursor-pointer' onClick={() => setColor("yellow")} >Yellow</button>
